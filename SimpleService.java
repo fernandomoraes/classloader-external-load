2b7d535b55e531db885f44f0083a4ec2589d0964
Fernando Moraes <fernandosdemoraes@gmail.com>
2015-09-20 00:14:45 -0300

    update service name

--- SimpleService.java
+++ SimpleService.java
@@ -1,4 +1,4 @@
-public class Fernando implements com.github.fernandomoraes.services.api.Executable {
+public class SimpleService implements com.github.fernandomoraes.services.api.Executable {
 	
 	public String execute () {
 		return "serviço rodando do tmp, fui ver seriado!!!";
